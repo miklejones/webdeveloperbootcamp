fe82e059c5b7f5127c9d23f931d16397563fd0c2
Michael Garland <mgarland@pmgnet.local>
2019-01-04 13:10:17 -0500

    working on arrays

---
+++ Section-13-JS-Basics-Arrays/Todo-List-Code-Along/todo.js
@@ -0,0 +1,21 @@
+window.setTimeout(function () {
+    // ALL JAVASCRIPT GOES HERE
+    var todos = ['buy new turtle'];
+
+    var input = prompt("What would you like to do?");
+
+    while (input !== 'quit') {
+
+        if (input === "list") {
+            console.log(todos);
+        } else if (input === 'new') {
+            var newTodo = prompt("Enter new todo");
+            //add to todos array
+            todos.push(newTodo);
+        }
+        var input = prompt("What would you like to do?");
+
+    }
+    console.log("OK, you quit the app.");
+
+}, 500);
