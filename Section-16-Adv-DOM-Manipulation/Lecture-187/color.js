7b02ba0ea8704dfc5d5875121db3710530e1e44a
Michael Garland <michaelcgarland@gmail.com>
2019-01-14 17:02:25 -0500

    bad formatting

---
+++ Section-16-Adv-DOM-Manipulation/Lecture-187/color.js
@@ -0,0 +1,18 @@
+console.log("its alive bitch");
+
+var button = document.querySelector("button");
+// var isPurple = false;
+
+// button.addEventListener("click", function () {
+//     if (!isPurple) {
+//         document.body.style.background = "purple";
+//         isPurple = true;
+//     } else {
+//         document.body.style.background = "white";
+//         isPurple = false;
+//     };
+// })
+
+button.addEventListener("click", function () {
+    document.body.classList.toggle("purple");
+})
---
+++ Section-16-Adv-DOM-Manipulation/Score-Keeper-Project/score-keeper.js
@@ -0,0 +1,10 @@
+var p1Button = document.querySelector("#p1");
+var p2Button = document.querySelector("#p2");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var p1Score = 0;
+
+p1Button.addEventListener("click", function () {
+    p1Score++;
+
+})
