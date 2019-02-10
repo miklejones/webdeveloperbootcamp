f4844ef2e9810bcfcfe387610cc3ea3c6fb8932f
Michael Garland <michaelcgarland@gmail.com>
2019-02-10 16:08:08 -0500

    moving to user auth

--- Yelp-Camp/v5/app.js
+++ Yelp-Camp/v5/app.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 mongoose.connect("mongodb://localhost:27017/yelp_camp_v3", { useNewUrlParser: true })
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"))
 seedDB();
 
 
